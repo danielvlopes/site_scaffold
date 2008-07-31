84f38beafbbf5ba7224255d24d9889636ca1211d
Daniel Lopes <danielvlopes@gmail.com>
2008-07-31 13:23:17 -0300

    fix products_helper for show price

--- generators/products_catalog/templates/helpers/products_helper.rb
+++ generators/products_catalog/templates/helpers/products_helper.rb
@@ -1,12 +1,17 @@
 module ProductsHelper
 	
 	def show_price(product)
-		if product.promotional_price > 0
-			html = "de <span style='text-decoration:line-through;'>#{product.price}</span><br/>"
-			html += "por <span class='promotional_price'>#{product.promotional_price}</span>"
-		else
-			"<b>#{product.price}</b>"
-		end
+  	if !product.promotional_price.blank?
+  	  html = "" 
+  		html += "de <span style='text-decoration:line-through;'>#{product.price}</span>" if !product.price.blank?
+  		html += " por <span class='promotional_price'>#{product.promotional_price}</span>"
+  	else
+  		if !product.price.blank?
+  			"#{product.price}"
+  		else 
+  			"a consultar"
+  		end		
+  	end
 	end
 	
 end
